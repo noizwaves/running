768927329046001a03529a3a6a09e80a7149f0ac
Adam Neumann <noizwaves@users.noreply.github.com>
2021-07-10 13:42:51 -0600

    Show list of runs

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,15 +1,59 @@
 const express = require('express')
 const path = require('path')
+const {DateTime} = require('luxon')
+const {readdir, readFile} = require('fs').promises
+const fitDecoder = require('fit-decoder')
+
+//
+// FIT data extraction
+//
+interface RunSummary {
+  startTime: typeof DateTime
+  totalDistance: number
+  totalTime: number
+  avgHeartRate: number
+  avgSpeed: number
+  avgCadence: number
+}
+
+const extractSummary = async (filePath: string): Promise<RunSummary> => {
+  const file = await readFile(filePath)
+
+  const jsonRaw = fitDecoder.fit2json(file.buffer)
+  const json = fitDecoder.parseRecords(jsonRaw)
+
+  const startTime = DateTime.fromJSDate(fitDecoder.getRecordFieldValue(json, 'session', 'start_time')[0])
+  const totalDistance = fitDecoder.getRecordFieldValue(json, 'session', 'total_distance')[0]
+  const totalTime = fitDecoder.getRecordFieldValue(json, 'session', 'total_timer_time')[0] / 1000
+  const avgHeartRate = fitDecoder.getRecordFieldValue(json, 'session', 'avg_heart_rate')[0]
+  const avgSpeed = fitDecoder.getRecordFieldValue(json, 'session', 'avg_speed')[0]
+  const avgCadence = fitDecoder.getRecordFieldValue(json, 'session', 'avg_cadence')[0]
+
+  return {startTime, totalDistance, totalTime, avgSpeed, avgHeartRate, avgCadence}
+}
 
 //
 // Application
 //
-const buildApplication = ({}) => {
+const buildApplication = ({runsRootPath}) => {
   const app = express()
 
-  app.get('/hello', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({hello: 'world'}))
+  app.get('/api/runs', async (req, res) => {
+    const runFilenames = await readdir(runsRootPath)
+
+    const runs: any = await Promise.all(runFilenames.map(async (filename: string) => {
+      const filePath = path.join(runsRootPath, filename)
+      const summary = await extractSummary(filePath)
+      return {
+        ...summary,
+        id: filename,
+      }
+    }))
+
+    runs.sort((a, b) => a.startTime - b.startTime).reverse()
+
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify({runs: runs}))
   })
 
   app.use('/', express.static(path.join(__dirname, '../../dist/frontend/')))
@@ -23,12 +67,13 @@ const buildApplication = ({}) => {
 // Configuration
 //
 const PORT = process.env.PORT || 3000;
+const runsRootPath = path.resolve(process.env.RUNS_ROOT_PATH || 'fixtures/runs')
 
 
 //
 // Bootstrap the application
 //
-const app = buildApplication({})
+const app = buildApplication({runsRootPath})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from 'react'
+import axios from 'axios'
+import {DateTime, Duration} from 'luxon'
 
-const App = () => {
-  return (
-    <div>
-      Hello, world!
-    </div>
-  );
+const parseRuns = (raw) => {
+  return raw.map(rawRun => {
+    return {
+      ...rawRun,
+      startTime: DateTime.fromISO(rawRun.startTime)
+    }
+  })
 }
 
-export default App;
+const App = () => {
+  const [runs, setRun] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get('/api/runs')
+      .then(response => {
+        setRun(parseRuns(response.data.runs))
+      })
+  }, [])
+
+  const renderDistance = (totalDistance) => {
+    const numeral = (totalDistance / 1000).toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})
+    return `${numeral} km`
+  }
+
+  const renderDuration = (totalTime) => {
+    const duration = Duration.fromObject({seconds: totalTime})
+    return duration.toFormat('mm:ss')
+  }
+
+  const renderRunRows = () => {
+    return runs.map((run) => {
+      return (
+        <tr key={run.id}>
+          <td>{run.startTime.toFormat('ccc, M/d/yyyy')}</td>
+          <td>{renderDuration(run.totalTime)}</td>
+          <td>{renderDistance(run.totalDistance)}</td>
+        </tr>
+      )
+    })
+  }
+
+  const renderRuns = () => {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Distance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {renderRunRows()}
+        </tbody>
+      </table>
+    )
+  }
+
+  return (
+    <div>
+      {renderRuns()}
+    </div>
+  )
+}
+
+export default App
