51aa2b3cd92e467d753a1bc19334bb4b953d3fbc
Adam Neumann <noizwaves@users.noreply.github.com>
2021-07-17 12:16:59 -0600

    Fix error on every auto reload

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from "react-router-dom"
 import App from './App'
 
+if (module.hot) {
+  module.hot.accept()
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
