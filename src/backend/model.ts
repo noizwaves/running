813b159e953cd67a6b73736364380c6a02521209
Adam Neumann <noizwaves@users.noreply.github.com>
2021-07-30 19:24:01 -0600

    Enhance jsdocs for run summary model

--- src/backend/model.ts
+++ src/backend/model.ts
@@ -3,13 +3,22 @@ import { DateTime}  from 'luxon'
 export type RunId = string
 
 export interface RunSummary {
+  /** When the run started */
   startTime: DateTime
 
   /** Distance of the run (in meters) */
   totalDistance: number
+
+  /** Duration of the run (in seconds) */
   totalTime: number
+
+  /** Average heart rate for the run (in beats per minute) */
   avgHeartRate: number
+
+  /** Average speed of the run (in m/s) */
   avgSpeed: number
+
+  /** Average cadence of the run (in steps per minute) */
   avgCadence: number
 }
 
